74d06f0c3e0edc7c2509dd24890f6b8925b2569c
CoKatharina <cokatha@freenet.de>
2024-10-14 16:33:00 +0200

    Update test_method.py
    Tests for compute_b and compute_A

--- test_method.py
+++ test_method.py
@@ -21,14 +21,18 @@ class TestIdentity(unittest.TestCase):
         self.C = Point(1,2)
         self.D = Point(0,1)
 
-        self.delta_x = 1/4
+        #self.delta_x = 1/4
+        self.delta_x = 1/3
         self.delta_y = 1/2
 
-        self.boundary_cond_types = ['Dirichlet', 'Neumann', 'Dirichlet', 'Dirichlet']
+        self.boundary_cond_types = [['Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet'], ['Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann', 'Neumann'], ['Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet'], ['Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet']]
         self.boundary_cond_values = [[15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15], [40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40]]
 
-        self.nx = 9
+        #self.nx = 9
+        self.nx = 7
         self.ny = 3
+
+        self.n_total = self.nx * self.ny
 
         self.problem = Problem(
             id=1,
@@ -45,12 +49,31 @@ class TestIdentity(unittest.TestCase):
 
     def test_compute_b(self):
 
-        data_b_expected = [40, 15, 15, 15, 15, 15, 15, 15, 15, 40, 0, 15, 15, 15, 15, 15, 15, 15, 15, 15]
-        rows_b_expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+        #data_b_expected = [40, 15, 15, 15, 15, 15, 15, 15, 15, 40, 0, 15, 15, 15, 15, 15, 15, 15, 15, 15]
+        #rows_b_expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+        #cols_b_expected = np.zeros((len(rows_b_expected)))
+
+        #b_sparse_expected = csr_matrix((data_b_expected, (rows_b_expected, cols_b_expected)), shape=(27,1))
+
+        data_b_expected = [40, 15, 15, 15, 15, 15, 15, 40, 0, 15, 15, 15, 15, 15, 15, 15]
+        rows_b_expected = [0, 1, 2, 3, 4, 5, 6, 7, 13, 14, 15, 16, 17, 18, 19, 20]
         cols_b_expected = np.zeros((len(rows_b_expected)))
 
-        b_sparse_expected = csr_matrix((data_b_expected, (rows_b_expected, cols_b_expected)), shape=(27,1))
+        b_sparse_expected = csr_matrix((data_b_expected, (rows_b_expected, cols_b_expected)), shape=(self.n_total,1))
 
         b_sparse = self.method.compute_b(self.problem)
 
         np.testing.assert_array_equal(b_sparse.toarray(), b_sparse_expected.toarray())
+
+
+    def test_compute_A(self):
+
+        data_A_expected = [1, 1, 1, 1, 1, 1, 1, 1, -26, 9, 9, 4, 4, -26, 9, 9, 4, 4, -26, 9, 9, 4, 4, -26, 9, 9, 4, 4, -26, 9, 9, 4, 4, 17, -9, -4, -4, 1, 1, 1, 1, 1, 1, 1]
+        rows_A_expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 13, 13, 13, 13, 14, 15, 16, 17, 18, 19, 20]
+        cols_A_expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 9, 1, 15, 9, 8, 10, 2, 16, 10, 9, 11, 3, 17, 11, 10, 12, 4, 18, 12, 11, 13, 5, 19, 13, 12, 20, 6, 14, 15, 16, 17, 18, 19, 20]
+
+        A_sparse_expected = csr_matrix((data_A_expected, (rows_A_expected, cols_A_expected)), shape=(self.n_total,self.n_total))
+
+        A_sparse = self.method.compute_A(self.problem)
+
+        np.testing.assert_array_equal(A_sparse.toarray(), A_sparse_expected.toarray())
